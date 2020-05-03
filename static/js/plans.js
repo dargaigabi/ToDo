4053cbcd2364cab48c433079f5cec9a792a8cbe0
dargaigabi <dargai.gabi@gmail.com>
2020-05-03 11:25:00 +0200

    Amount Allocation

--- static/js/plans.js
+++ static/js/plans.js
@@ -12,9 +12,22 @@ $('.form-control').change(function(){
             var type_id = response['type_id']
             var original_amount = parseInt($('#' + type_id).val())
             $('#' + type_id).val(original_amount + amount)
+            countAllocation();
         }
     });
 })
+
+function countAllocation() {
+    var recurring_expenses = parseInt($('#1').val());
+    var one_time_expenses = parseInt($('#2').val());
+    var recurring_incomes = parseInt($('#3').val());
+    var one_time_incomes = parseInt($('#4').val());
+    var recurring_savings = parseInt($('#5').val());
+    var one_time_savings = parseInt($('#6').val());
+    var amount_to_allocate = recurring_incomes + one_time_incomes - recurring_expenses - one_time_expenses - recurring_savings - one_time_savings;
+    $('#amount_to_allocate').val(amount_to_allocate);
+}
+
 
 $('.period_selector').change(function() {
     var field_id=$(this).children(':selected').data('id');
@@ -37,5 +50,6 @@ $(document).ready(function() {
         var original_amount = parseInt($('#' + type_id).val());
         var amount = parseInt($(this).val());
         $('#' + type_id).val(original_amount + amount);
+        countAllocation();
     })
 })
